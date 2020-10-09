0c441ba2c4051e260b47deef5dcbefdee4c4c18e
marekboro <marek.borowicz83@gmail.com>
2020-10-09 16:37:04 +0100

    added UI integration tests

--- README.md
+++ README.md
@@ -11,9 +11,9 @@
 
 ##### UI INTEGRATION TESTING: 
 
-- [ ] Do the number buttons update the display of the running total?
-- [ ] Do the arithmetical operations update the display with the result of the operation?
-- [ ] Can multiple operations be chained together?
+- [x] Do the number buttons update the display of the running total?
+- [x] Do the arithmetical operations update the display with the result of the operation?
+- [x] Can multiple operations be chained together?
 - [ ] Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
 - [ ] What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct - - [ ] the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
 
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,28 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display with the result of the arithmetical operation', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number7').click();
+    cy.get('#number8').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '80')
+  })
+
+  it('should chained together multiple operations', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number4').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+
 })
