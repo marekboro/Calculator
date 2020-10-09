2b0687e230c37a21453c11cdde65db17fd53c05a
marekboro <marek.borowicz83@gmail.com>
2020-10-09 17:07:57 +0100

    added a few more tests, including verifying the message upon division by 0: Infinity

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 - [x] Do the number buttons update the display of the running total?
 - [x] Do the arithmetical operations update the display with the result of the operation?
 - [x] Can multiple operations be chained together?
-- [ ] Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+- [x] Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
 - [ ] What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct - - [ ] the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
 
 ## Project setup
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -32,5 +32,64 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '2')
   })
 
+  it('should be able to give decimals', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2.5')
+  })
+
+  it('should be able to result in a negative number', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-4')
+  })
+
+  it('should be able to result  16 decimal spaces', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0.6666666666666666')
+  })
+
+  it('should be able to display very larget numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#number6').click();
+    cy.get('#number7').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number8').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number7').click();
+    cy.get('#number9').click();
+    cy.get('#number6').click();
+    cy.get('#operator_equals').click();
+    // cy.get('.display').should('contain', '21024340332')
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number3').click();
+    cy.get('#number8').click();
+    cy.get('#number5').click();
+    cy.get('#number7').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1973281510540524')
+    
+  })
+
+  it('should not be able to divide by zero', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', 'Infinity')
+  })
+
+
 
 })
